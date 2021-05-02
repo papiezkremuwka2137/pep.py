fb1956aa2c42199d4bcca57d890097d52f95b62e
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2021-05-02 22:57:34 +0100

    BRUH

--- handlers/apiAerisThing.py
+++ handlers/apiAerisThing.py
@@ -1,6 +1,7 @@
 from objects import glob
 import tornado.web
 import tornado.gen
+from common.web import requestsManager
 import json
 import random
 
@@ -11,7 +12,7 @@ class handler(requestsManager.asyncRequestHandler):
         """Handles the server info endpoint for the Aeris client."""
         resp_dict = {
             "version": 0,
-            "motd": f"RealistikOsu | " + random.choice(glob.banchoConf.config['Quotes']),
+            "motd": f"RealistikOsu\n" + random.choice(glob.banchoConf.config['Quotes']),
             "onlineUsers": len(glob.tokenList),
             "icon": "https://ussr.pl/static/image/newlogo2.png",
             "botID": 999
